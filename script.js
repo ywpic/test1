79e34bf88d92aec930c4784396aafcb67e7f5174
ywpic <ywjcnu22@gmail.com>
2026-06-15 21:07:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const speech = document.getElementById("speech");
 const timer = document.getElementById("timer");
+const character = document.getElementById("character");
+
+const joke = {
+
+    question : "낙타의 엄마는?",
+
+    answer : "늑대!",
+
+    explain : "(늑대가 낙타났다!)"
+};
+
+speech.textContent = joke.question;
 
 let count = 10;
 
@@ -9,18 +21,25 @@ const interval = setInterval(() => {
 
     timer.textContent = count;
 
-    if(count === 0){
+    if(count <= 0){
 
         clearInterval(interval);
 
         timer.style.display = "none";
 
         speech.innerHTML = `
-            <div>
-                <h2>늑대!</h2>
-                <p>(늑대가 낙타났다!)</p>
+            <div>${joke.answer}</div>
+            <div style="
+                font-size:1rem;
+                margin-top:10px;
+                font-weight:normal;
+            ">
+                ${joke.explain}
             </div>
         `;
+
+        character.src =
+        "images/character-laugh.png";
     }
 
 },1000);
